e42ca7656373c5e21e61d2ed9dc2e25cdddb1fb1
ar20190114 <skdr0724@gmail.com>
2022-07-31 17:02:25 +0900

    report3

--- No.2/index3.py
+++ No.2/index3.py
@@ -3,46 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 初期値
-N = np.random.rand(10000, 2)
-W = np.random.rand(4, 4, 2)
+N = np.random.rand(1500, 3)
+W = np.random.rand(4, 4, 3)
 alpha = 0.05
 
 i = 1
+Colors = ['red', 'blue', 'green', 'pink']
 while True:
 
-    if i % 20 == 0:
+    if i % 3 == 0:
         X = []
         Y = []
         for k in W:
             for l in k:
                 X.append(float(l[0]))
                 Y.append(float(l[1]))
-        plt.scatter(X, Y)
-        plt.show()
+        # plt.title("{}".format(i))
+        # plt.scatter(X, Y, color = Colors[i//3 - 1])
+        # plt.show()
     
     for (j, n) in enumerate(N):
         min_index = np.argmin(((W - n)**2).sum(axis=2))
         mini = int(min_index / 4)
         minj = int(min_index % 4)
 
-        W[mini][minj] = (1 - alpha) * W[mini][minj] + alpha * n
+        W[mini, minj] = (1 - alpha) * W[mini, minj] + alpha * n
 
         for a in range(-1, 2):
             for b in range(-1, 2):
-                if a == 0 and b == 0:
-                    continue
-                elif (mini + a < 0):
-                    continue
-                elif (mini + a > 3):
-                    continue
-                elif (minj + b < 0):
-                    continue
-                elif (minj + b > 3):
-                    continue
-                else:
-                    W[mini+a][minj+b] = (1 - alpha/2) * W[mini][minj] + alpha/2 * n
+                try:
+                    if a == 0 and b == 0:
+                        continue
+                    else:
+                        W[mini+a, minj+b] = (1 - alpha/2) * W[mini, minj] + alpha/2 * n
+                except:
+                    pass
 
-    if i == 100:
+    if i == 12:
         break
 
     i += 1
+
+print("imgshow")
+im = plt.imshow(W,interpolation='none')
+plt.show()
